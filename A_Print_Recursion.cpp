9733469f0b102cb0af8d40089a58bd41424db4d2
NourElDienNwara <noureldiennwara312@gmail.com>
2026-02-25 16:36:44 +0200

    A. Print Recursion

---
+++ A_Print_Recursion.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+ 
+void printRecusion(int n) 
+{
+    if (n == 0) return;
+    cout << "I love Recursion" << endl;
+    printRecusion(n - 1);
+}
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    printRecusion(n);
+}
