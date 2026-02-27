245cc35d6a927e286070b4fc3a7af50bfaad9187
NourElDienNwara <noureldiennwara312@gmail.com>
2026-02-27 21:13:56 +0200

    C. Print from N to 1

---
+++ C_Print_from_N_to_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void printNto1(int n)
+{
+    if(n == 0) return;
+    if (n == 1) cout << n;
+    else cout << n << " ";
+    printNto1(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printNto1(n);
+}
